e6da6f50635fd1ff994be6ed1cb7471cb38004d0
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2024-08-01 21:00:50 -0700

    change to spaces

--- src/app/blog/introducing-start-up-engineer-library/container.tsx
+++ src/app/blog/introducing-start-up-engineer-library/container.tsx
@@ -2,7 +2,7 @@ import { Link } from "@/components/link";
 import { Text } from "@/components/text";
 
 export const Container = () => (
-	<article>
+  <article>
     <div className="mb-6">
       <Text className="text-2xl font-bold" tag="h1">
         Introducing the Start-up Engineering Library 📚
@@ -11,42 +11,42 @@ export const Container = () => (
         8/1/2024
       </Text>
     </div>
-		<Text className="mb-6">
-			I’m very excited to share a small project I’ve put together, the{" "}
-			<Link href="https://github.com/jpbullalayao/startup-engineer-library">
-				Start-up Engineer Library
-			</Link>
-			!
-		</Text>
+    <Text className="mb-6">
+      I’m very excited to share a small project I’ve put together, the{" "}
+      <Link href="https://github.com/jpbullalayao/startup-engineer-library">
+        Start-up Engineer Library
+      </Link>
+      !
+    </Text>
 
-		<Text className="mb-6">
-			You can probably guess the target audience from the name, but I hope it
-			can reach software engineers of any level, whether you’re just starting
-			your career out of school and exploring the world of start-ups, or if
-			you’re a principal engineer ready to start on the next big idea.
-		</Text>
+    <Text className="mb-6">
+      You can probably guess the target audience from the name, but I hope it
+      can reach software engineers of any level, whether you’re just starting
+      your career out of school and exploring the world of start-ups, or if
+      you’re a principal engineer ready to start on the next big idea.
+    </Text>
 
-		<Text className="mb-6">
-			In this library, I’ve begun to compile a list of some of the highest
-			quality resources out there that have proven to be invaluable during my
-			career as an engineer building start-ups. I find myself constantly coming
-			back to these when I’m building and ideating, and I find these resources
-			just as useful as the last time I’ve consumed them.
-		</Text>
+    <Text className="mb-6">
+      In this library, I’ve begun to compile a list of some of the highest
+      quality resources out there that have proven to be invaluable during my
+      career as an engineer building start-ups. I find myself constantly coming
+      back to these when I’m building and ideating, and I find these resources
+      just as useful as the last time I’ve consumed them.
+    </Text>
 
-		<Text className="mb-6">
-			The resources published in the library range from a few different mediums
-			- namely, YouTube videos and online essays. As I continue growing and
-			pursuing my goals, you can expect more content from this library that not
-			only takes the form of videos and essays, but also podcasts, book
-			recommendations, and blog posts. I hope it can be an invaluable resource
-			to you in your role as a software engineer building start-ups.
-		</Text>
+    <Text className="mb-6">
+      The resources published in the library range from a few different mediums
+      - namely, YouTube videos and online essays. As I continue growing and
+      pursuing my goals, you can expect more content from this library that not
+      only takes the form of videos and essays, but also podcasts, book
+      recommendations, and blog posts. I hope it can be an invaluable resource
+      to you in your role as a software engineer building start-ups.
+    </Text>
 
-		<Text className="mb-6">
-			Please feel free to star/watch the repo, and tweet me{" "}
-			<Link href="https://twitter.com/professorragna">@professorragna</Link> if
-			there are any particular resources you know of that I could learn from!
-		</Text>
-	</article>
+    <Text className="mb-6">
+      Please feel free to star/watch the repo, and tweet me{" "}
+      <Link href="https://twitter.com/professorragna">@professorragna</Link> if
+      there are any particular resources you know of that I could learn from!
+    </Text>
+  </article>
 );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,131 +10,131 @@ import { Text } from "@/components/text";
 import { TipSection } from "./_components/tip-section";
 
 export const metadata: Metadata = {
-	title: "Professor Ragna - Start-ups, Engineering, & Video Games",
-	description: "The home page for Professor Ragna (Jourdan Bul-lalayao)",
+  title: "Professor Ragna - Start-ups, Engineering, & Video Games",
+  description: "The home page for Professor Ragna (Jourdan Bul-lalayao)",
 };
 
 const posts = [
-	{
-		title: "Introducing the Start-up Engineering Library",
-		href: "/blog/introducing-start-up-engineer-library",
-		date: "8/1/2024",
-	},
+  {
+    title: "Introducing the Start-up Engineering Library",
+    href: "/blog/introducing-start-up-engineer-library",
+    date: "8/1/2024",
+  },
 ];
 
 const Home = () => {
-	return (
-		<div className="flex justify-center">
-			<div className="w-full lg:w-[70%]">
-				<Header />
+  return (
+    <div className="flex justify-center">
+      <div className="w-full lg:w-[70%]">
+        <Header />
 
-				<main className="flex justify-center my-7 lg:my-[80px]">
-					<div className="w-full lg:w-1/2">
-						<Section>
-							<h1 className="text-2xl font-bold">{`Hello, I'm Jourdan 👾`}</h1>
+        <main className="flex justify-center my-7 lg:my-[80px]">
+          <div className="w-full lg:w-1/2">
+            <Section>
+              <h1 className="text-2xl font-bold">{`Hello, I'm Jourdan 👾`}</h1>
 
-							<Text className="my-6">
-								I build start-ups, web apps and APIs.
-							</Text>
+              <Text className="my-6">
+                I build start-ups, web apps and APIs.
+              </Text>
 
-							<Text className="my-6">
-								{`I'm a full-stack software engineer based in the San Francisco
+              <Text className="my-6">
+                {`I'm a full-stack software engineer based in the San Francisco
                 Bay Area, with a decade of experience building and deploying web
                 apps and APIs from zero.`}
-							</Text>
+              </Text>
 
-							<Text className="my-6">
-								{`I've built all types of start-ups, from pre-seed with 0 employees, to mid-stage that IPO'ed during my tenure.`}
-							</Text>
+              <Text className="my-6">
+                {`I've built all types of start-ups, from pre-seed with 0 employees, to mid-stage that IPO'ed during my tenure.`}
+              </Text>
 
-							<Text className="my-6">
-								{`I'm still figuring out what I want to focus on next. In the mean time, I'm researching, prototyping and studying to become the best product engineer
+              <Text className="my-6">
+                {`I'm still figuring out what I want to focus on next. In the mean time, I'm researching, prototyping and studying to become the best product engineer
                 I can be 🛠️📚. I'm also on `}
-								<Link href="https://twitter.com/professorragna">Twitter</Link> a
-								lot.
-							</Text>
-						</Section>
+                <Link href="https://twitter.com/professorragna">Twitter</Link> a
+                lot.
+              </Text>
+            </Section>
 
-						<Section>
-							<h2 className="text-2xl font-bold">Video Games 🎮</h2>
+            <Section>
+              <h2 className="text-2xl font-bold">Video Games 🎮</h2>
 
-							<Text className="my-6">
-								{`When I'm not coding, I `}
-								<Link href="https://twitch.tv/professorragna">stream</Link> and
-								talk Pokemon, fighting games 🐬, and JRPGs.
-							</Text>
+              <Text className="my-6">
+                {`When I'm not coding, I `}
+                <Link href="https://twitch.tv/professorragna">stream</Link> and
+                talk Pokemon, fighting games 🐬, and JRPGs.
+              </Text>
 
-							<Text className="my-6">
-								I enjoy playing some games competitively. I was a 2023 World
-								Championship competitor in Pokemon, and am recently also a 2024
-								World Championship invitee.
-							</Text>
+              <Text className="my-6">
+                I enjoy playing some games competitively. I was a 2023 World
+                Championship competitor in Pokemon, and am recently also a 2024
+                World Championship invitee.
+              </Text>
 
-							<Text className="my-6">
-								{`Casually, I'll wind down with a good RPG like Final Fantasy. I'm on the Dawntrail hype, catch me on Behemoth in `}
-								<Link href="https://na.finalfantasyxiv.com/lodestone/character/3410408/#profile">
-									FFXIV
-								</Link>
-								.
-							</Text>
-						</Section>
+              <Text className="my-6">
+                {`Casually, I'll wind down with a good RPG like Final Fantasy. I'm on the Dawntrail hype, catch me on Behemoth in `}
+                <Link href="https://na.finalfantasyxiv.com/lodestone/character/3410408/#profile">
+                  FFXIV
+                </Link>
+                .
+              </Text>
+            </Section>
 
-						<Section>
-							<h2 className="text-2xl font-bold">Start-ups & Engineering 🚀</h2>
+            <Section>
+              <h2 className="text-2xl font-bold">Start-ups & Engineering 🚀</h2>
 
-							<Text className="my-6">
-								Are you looking for an engineer for your next start-up?
-							</Text>
+              <Text className="my-6">
+                Are you looking for an engineer for your next start-up?
+              </Text>
 
-							<Text className="my-6">
-								Tweet me{" "}
-								<Link href="https://twitter.com/professorragna">
-									@professorragna
-								</Link>{" "}
-								or <Link href="mailto:professor.ragna@gmail.com">e-mail</Link>{" "}
-								{`me. I'd love to hear what you're building.`}
-							</Text>
-						</Section>
+              <Text className="my-6">
+                Tweet me{" "}
+                <Link href="https://twitter.com/professorragna">
+                  @professorragna
+                </Link>{" "}
+                or <Link href="mailto:professor.ragna@gmail.com">e-mail</Link>{" "}
+                {`me. I'd love to hear what you're building.`}
+              </Text>
+            </Section>
 
-						<Section>
-							<h2 className="text-2xl font-bold">Chat with me ☕</h2>
+            <Section>
+              <h2 className="text-2xl font-bold">Chat with me ☕</h2>
 
-							<Text className="my-6">
-								{`I'm always open to meeting other engineers, founders, and fellow gamers. `}
-								<Link href="https://calendly.com/professorragna/30min">
-									Schedule a chat
-								</Link>{" "}
-								with me some time and let me know how I can help!
-							</Text>
-						</Section>
+              <Text className="my-6">
+                {`I'm always open to meeting other engineers, founders, and fellow gamers. `}
+                <Link href="https://calendly.com/professorragna/30min">
+                  Schedule a chat
+                </Link>{" "}
+                with me some time and let me know how I can help!
+              </Text>
+            </Section>
 
-						<Section>
-							{/* Suspense needed for useSearchParams... https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
-							<Suspense>
-								<TipSection />
-							</Suspense>
-						</Section>
+            <Section>
+              {/* Suspense needed for useSearchParams... https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+              <Suspense>
+                <TipSection />
+              </Suspense>
+            </Section>
 
-						<Section>
-							<h2 className="text-2xl font-bold">Blog ✍️</h2>
+            <Section>
+              <h2 className="text-2xl font-bold">Blog ✍️</h2>
 
-							{posts.map(({ title, href, date }) => (
-								<div key={href} className="flex justify-between my-6">
-									<Text>
-										<Link href={href}>{title}</Link>
-									</Text>
+              {posts.map(({ title, href, date }) => (
+                <div key={href} className="flex justify-between my-6">
+                  <Text>
+                    <Link href={href}>{title}</Link>
+                  </Text>
 
-									<Text className="text-secondary">{date}</Text>
-								</div>
-							))}
-						</Section>
-					</div>
-				</main>
+                  <Text className="text-secondary">{date}</Text>
+                </div>
+              ))}
+            </Section>
+          </div>
+        </main>
 
-				<Footer />
-			</div>
-		</div>
-	);
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
