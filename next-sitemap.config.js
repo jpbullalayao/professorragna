c0a3ebe940dc54c82f18b03ff04af7b6e381cb94
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2024-08-01 23:05:12 -0700

    Add Sitemap (#12)

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://www.professorragna.com",
+  changefreq: "daily",
+  exclude: ["/opengraph-image.jpg", "/twitter-image.jpg"],
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+}
